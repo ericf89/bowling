c4c48118afb7e1e6739d7e04b8449dff217c759c
Eric Fennell <ericf89@gmail.com>
2014-10-20 17:09:23 -0500

    Cleaned up the scoring service and associated tests a bunch.  Looking much better!

--- services/scoringService.js
+++ services/scoringService.js
@@ -1,25 +1,87 @@
 
 var _ = require('underscore');
-
+// Sum should be pretty self explanatory.... 
 var sum = function (a, b) { return a + b;}; 
 
+/*	This is a helper object to help with score calculation.
+	Each frame is capable of calculating its score, because it
+	includes not only its own rolls, but also the rolls of
+	future frames if necessary for calculation. 
+
+	If there there aren't enough rolls for the frame to calculate it's score,
+	its isComplete() will return false; 
+*/
+var ScoreFrame = function(rolls){
+	this.rolls = rolls; 
+	this.isComplete = function(){
+		if(this.isStrike() || this.isSpare()) return this.rolls.length === 3;
+		return this.rolls.length === 2; 
+	};
+
+	this.isStrike = function(){
+		return this.rolls[0] === 10; 
+	};
+
+	this.isSpare = function(){
+		return this.rolls[0] + this.rolls[1] === 10; 
+	};
+	this.score = function(){
+		return _.reduce(this.rolls, sum); 
+	}; 
+};
+
 exports.getScoreAtFrame = function(rollScoreArray, frameIndex){
-	var score = 0 ; 
-	for(var i = 0; i < frameIndex; i++){
-		if(rollScoreArray[i] === 10){
-			if(rollScoreArray.length < i+2) return null;
-			score += _.reduce(rollScoreArray.slice(i, i+3), function(a, b){ return a+b;}); 
-		}
-	}
+	var score = 0,
+		i = 0,
+		frames = exports.importRollsToScoreFrames(rollScoreArray);
+	
+	frameIndex = frameIndex || frames.length;
 
+	/*	Let's first check to make sure that this is a valid frame get a score for.
+		If we don't have enough data for calculating score at the requested frame
+		because of strikes or spares,  we'll return null;
+	*/
+	if(!frames[frameIndex-1] || !frames[frameIndex-1].isComplete())
+		return null; 
+	
 
-	for(var i = 0; i < throwsToCount; i++)
-	{
-		score += rollScoreArray[i]; 
+	/*	Now that we've ruled out those edge cases we can assume that we have enough data
+		to calculate the score at the frame requested.  
+	*/ 
+	for(i=0; i < frameIndex; i++){
+		score += frames[i].score(); 
 	}
-	return score;
+ 
+	return score; 
+
 }; 
 
 exports.getTotalScore = function(rollScoreArray){
-	return exports.getScoreAtFrame(rollScoreArray, Math.floor(rollScoreArray.length/2));
+	return exports.getScoreAtFrame(rollScoreArray);
+};
+
+
+/*	This method accepts an array of rolls, (the number of pins knocked down...).  
+	It constructs an array of frames from these rolls, so that they can
+	be used to calculate score later.  
+*/ 
+exports.importRollsToScoreFrames = function(rollScoreArray){
+	var frames = []; 
+	//Iterate over all available roll data!
+	for(var i = 0; i < rollScoreArray.length;){
+		if(frames.length === 10) return frames; //This breaks us out when we reach the end of the game.
+		if(rollScoreArray[i] === 10){
+			frames.push(new ScoreFrame(rollScoreArray.slice(i, i+3)));// If value is a strike, we need the next 2 rolls for this frame!
+			i++;
+			continue;
+		}
+		if(rollScoreArray[i] + rollScoreArray[i + 1] === 10){  //If this roll, plus the next roll is 10, you have a spare!
+			frames.push(new ScoreFrame(rollScoreArray.slice(i, i+3))); //(Still i+3, because you need the two rolls that
+			i+=2;													   //that make a spare, plus one more!)
+			continue;
+		}
+		frames.push(new ScoreFrame(rollScoreArray.slice(i, i+2))); //Otherwise just an open frame. i+=2 to get to the next frame.
+		i+=2;
+	}
+	return frames; 
 };
--- specs/scoringService.spec.js
+++ specs/scoringService.spec.js
@@ -1,20 +1,122 @@
-var scoringService = require('../services/scoringService.js');
+var scoringService = require('../services/scoringService.js'),
+	_ = require('underscore');
 
-describe('The scoring service', function(){
-	
-	it('should correctly score an open frame.', function (){
-		var pinsKnockedDown = [3, 4]; 
-		expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(7);
+describe('In the scoring service,', function(){
+	describe('the importRollsToScoreFrames method', function(){
+		it('should import a series of open frames.', function(){
+			var pinsKnockedDown = [2, 3, 3, 6];
+			var frames = scoringService.importRollsToScoreFrames(pinsKnockedDown);
+			expect(frames.length).toBe(2);
+			_.each(frames, validateOpenFrame);
+		});
+		it('should import a series of frames with a spare.', function(){
+			var pinsKnockedDown = [7, 3, 3, 6, 3, 3];
+			var frames = scoringService.importRollsToScoreFrames(pinsKnockedDown);
+			expect(frames.length).toBe(3);
+			expect(frames[0].isComplete() && frames[0].isSpare()).toBe(true);
+			_.each(frames.slice(1, 3), validateOpenFrame); 
+		});
+		it('should import a series of frames with a strike.', function(){
+			var pinsKnockedDown = [10, 3, 6];
+			var frames = scoringService.importRollsToScoreFrames(pinsKnockedDown);
+			expect(frames.length).toBe(2); 
+			expect(frames[0].isComplete() && frames[0].isStrike()).toBe(true); 
+			validateOpenFrame(frames[1]);
+		});
+		it('should import a series of frames with strikes and spares', function(){
+			var pinsKnockedDown = [3, 7, 3, 2, 10, 3, 6, 10, 10, 3, 7];
+			var frames = scoringService.importRollsToScoreFrames(pinsKnockedDown);
+			expect(frames.length).toBe(7);
+			_.each([0, 6], function(i){
+				expect(frames[i].isSpare()).toBe(true);
+			});
+			_.each([2, 4, 5], function(i){
+				expect(frames[i].isStrike()).toBe(true);
+				expect(frames[i].isComplete()).toBe(true);
+			});
+			expect(frames[6].isComplete()).toBe(false); 
+		});
+		it('should import the frames in a perfect game.', function(){
+			var pinsKnockedDown = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+			var frames = scoringService.importRollsToScoreFrames(pinsKnockedDown);
+			expect(frames.length).toBe(10); 
+			_.each(frames, function(frame){
+				expect(frame.isComplete() && frame.isStrike()).toBe(true); 
+				expect(frame.isSpare()).toBe(false); 
+			});
+		});
+		it('should import a series of frames with a spare in the 10th frame.', function(){
+			var	pinsKnockedDown = [10, 10, 10, 10, 10, 10, 10, 10, 10, 3, 7, 5];
+			var frames = scoringService.importRollsToScoreFrames(pinsKnockedDown);
+			expect(frames.length).toBe(10);
+			_.each(frames.slice(0, 9), function(frame){
+				expect(frame.isComplete() && frame.isStrike()).toBe(true); 
+				expect(frame.isSpare()).toBe(false); 
+			});
+			expect(frames[9].isSpare() && frames[9].isComplete()).toBe(true); 
+		});
 	});
 
-	it('should correctly score a series of open frames.', function(){
-		var pinsKnockedDown = [3, 4, 5, 4]; 
-		expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(16);
+	describe('the getTotalScore method', function(){
+		it('should correctly score an open frame.', function (){
+			var pinsKnockedDown = [3, 4]; 
+			expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(7);
+		});
+
+		it('should correctly score a series of open frames.', function(){
+			var pinsKnockedDown = [3, 4, 5, 4]; 
+			expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(16);
+		});
+
+		it('should correctly score a spare.', function(){
+			var pinsKnockedDown = [3, 7, 5, 4];
+			expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(24);
+		});
+
+		it('should correctly score a strike.', function(){
+			var pinsKnockedDown = [10, 3, 6];
+			expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(28); 
+		});
+
+		it('should correctly score a perfect game.', function(){
+			var pinsKnockedDown = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]; 
+			expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(300); 
+		});
+
+		it('should correctly score all spares.', function(){
+			var pinsKnockedDown = [];
+			for(var i = 0; i < 21; i++) pinsKnockedDown.push(5);
+			expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(150); 
+		});
+
+		it('should correctly score spares and strikes.', function(){
+			var pinsKnockedDown = [3, 6, 5, 5, 10, 4, 4, 10, 10, 3, 6, 5, 5, 4, 4];
+			expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(128); 
+		});
+
+		it('should return the same value for the most recent frame.', function(){
+			var pinsKnockedDown = [3, 6]; 
+			expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(9); 
+			expect(scoringService.getScoreAtFrame(pinsKnockedDown, 1)).toBe(9);
+			expect(scoringService.getScoreAtFrame(pinsKnockedDown, 2)).toBe(null);
+		}); 
 	});
 
-	xit('should correctly score a spare.', function(){
-		var pinsKnockedDown = [3, 7, 5, 4];
-		expect(scoringService.getTotalScore(pinsKnockedDown)).toBe(16);
+	describe('the getScoreAtFrame method', function(){
+		it('should return null when requesting a frame without data', function(){
+			var pinsKnockedDown = [3, 5]; 
+			expect(scoringService.getScoreAtFrame(pinsKnockedDown, 5)).toBe(null);
+		});
+		it('should return null when requesting the score for a strike without data.', function(){
+			var pinsKnockedDown = [3, 5, 3, 6, 10];
+			expect(scoringService.getScoreAtFrame(pinsKnockedDown, 1)).toBe(8);
+			expect(scoringService.getScoreAtFrame(pinsKnockedDown, 2)).toBe(17);
+			expect(scoringService.getScoreAtFrame(pinsKnockedDown, 3)).toBe(null);
+		});
 	});
-
 });
+
+function validateOpenFrame(frame){
+	expect(frame.isComplete()).toBe(true);
+	expect(frame.isSpare() || frame.isStrike()).toBe(false);
+}
